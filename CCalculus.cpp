78e51314b1f1aa51a74b88a0e2b30f88ddd4ea11
Ariel Terkeltoub <ariter777@gmail.com>
2022-02-04 12:06:16 +0200

    Removed NaN usage.

--- CCalculus.cpp
+++ CCalculus.cpp
@@ -5,9 +5,10 @@ namespace libcalculus {
     template<>
     std::complex<double> Integrate(CFunction<std::complex<double>, std::complex<double>> const &f,
                                    CFunction<double, std::complex<double>> const &contour, double start, double end, double tol) {
-        std::complex<double> prev_result = NAN, result = NAN;
+        std::complex<double> prev_result, result;
+        size_t while_iters = 0;
         double n = 1 / tol;
-        while (isnanc(prev_result) || isnanc(result) || abs(result - prev_result) >= tol) {
+        while (while_iters < 2 || abs(result - prev_result) >= tol) {
             n *= 2;
             prev_result = result;
             result = 0.;
@@ -17,6 +18,7 @@ namespace libcalculus {
                 result += f(z) * (z - prev_z);
                 prev_z = z;
             }
+            ++while_iters;
         }
         return result;
     }
--- CCalculus.h
+++ CCalculus.h
@@ -2,8 +2,6 @@
 #include "CFunction.h"
 
 namespace libcalculus {
-    inline bool isnanc(std::complex<double> z) { return std::isnan(std::real(z)); }
-
     template<typename Dom, typename Ran, typename ContDom>
     Ran Integrate(CFunction<Dom, Ran> const &f, CFunction<ContDom, Dom> const &contour, ContDom start, ContDom End, double tol) = delete;
 }
