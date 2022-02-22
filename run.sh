014728297d0aaae032d91be2e2fc7f7297e4c9b5
Ariel Terkeltoub <ariter777@gmail.com>
2022-02-22 21:21:25 +0200

    Added line count to run.sh.

--- run.sh
+++ run.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
-python setup.py build_ext --inplace && python test.py
+set -e
+python setup.py build_ext --inplace
+echo "Successfully compiled $(wc -l $(git ls-files *.{h,cpp,py,pyx}) | tail -1 | sed -re 's/^\s*([0-9]+)\s+total\s*$/\1/g') lines."
+python test.py
--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@
 from setuptools import setup, Extension
 from Cython.Build import cythonize
 
-
 setup(ext_modules=cythonize(Extension("libcalculus", ["libcalculus.pyx"], extra_compile_args=["-std=c++17", "-O3", "-march=native"]), language_level=3, nthreads=4))
 
 import shutil
