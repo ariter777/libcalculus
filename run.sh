af4274fe022eb9620006b8b15b17a2d1e15f7636
Ariel Terkeltoub <ariter777@gmail.com>
2022-02-23 13:32:32 +0200

    Improved compilation.

--- run.sh
+++ run.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 set -e
-rm -f libcalculus.cpp
 python setup.py build_ext --inplace
 mkdir -p annotations
-mv *.html annotations
+find . -maxdepth 1 -type f -name '*.html' -quit | read && mv *.html annotations
 echo "Successfully compiled $(wc -l $(git ls-files *.{h,cpp,py,pyx}) | tail -1 | sed -re 's/^\s*([0-9]+)\s+total\s*$/\1/g') lines."
 python test.py
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, Extension
 from Cython.Build import cythonize
 
-setup(ext_modules=cythonize(Extension("libcalculus", ["libcalculus.pyx"], extra_compile_args=["-std=c++17", "-O3", "-march=native", "-msse", "-msse2", "-mfpmath=sse"]), language_level=3, nthreads=4, annotate=True))
+setup(ext_modules=cythonize(Extension("libcalculus", ["libcalculus.pyx"], extra_compile_args=["-std=c++17", "-O3", "-march=native", "-msse", "-msse2", "-mavx", "-mavx2", "-mfpmath=sse"]), language_level=3, nthreads=4, annotate=True))
 
 import shutil
 shutil.rmtree("build")
