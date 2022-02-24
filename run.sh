027962d931d67862ae30d6b0c0d4ed580f068e50
Ariel Terkeltoub <ariter777@gmail.com>
2022-02-24 20:39:50 +0200

    Added compilation options to run.sh.

--- run.sh
+++ run.sh
@@ -1,21 +1,35 @@
 #!/usr/bin/env bash
 set -e
+shopt -s extglob
 
-{ if [[ $@ > 1 && $1 == 'clean' ]]; then
+function clean {
   echo $'\e[92mCleaning.\e[0m'
   rm -vrf src/libcalculus.cpp build annotations 2>&1 | sed 's/^/    /g'
   echo
-fi ; } &> /dev/stderr
+}
 
+function linux_setup {
+  echo $'\e[92mBuilding.\e[0m'
+  { python setup.py build_ext --inplace
+  mkdir -p annotations
+  find src/ include/ -type f -name '*.html' | xargs -i -r mv {} annotations/ && echo "Annotations saved in annotations/"
+  echo "Successfully compiled $(wc -l src/!(libcalculus.cpp) include/* | tail -1 | sed -re 's/^\s*([0-9]+)\s+total\s*$/\1/g') lines."
+  echo ; } 2>&1 | sed 's/^/    /g' &> /dev/stderr
+}
 
-echo $'\e[92mBuilding.\e[0m'
-
-{ python setup.py build_ext --inplace
-mkdir -p annotations
-find src/ include/ -type f -name '*.html' | xargs -i -r mv {} annotations/ && echo "Annotations saved in annotations/"
-echo "Successfully compiled $(wc -l src/* include/* | tail -1 | sed -re 's/^\s*([0-9]+)\s+total\s*$/\1/g') lines."
-echo ; } 2>&1 | sed 's/^/    /g' &> /dev/stderr
-
+if [[ $# == 0 ]]; then # default build - linux
+  linux_setup
+else
+  while [[ $# > 0 ]]; do
+    case $1 in
+      'clean') clean ;;
+      'linux') linux_setup ;;
+      'windows') windows_setup ;;
+      *) echo "Unknown parameter: $1"; exit 1 ;;
+    esac
+    shift
+  done &> /dev/stderr
+fi
 
 echo $'\e[92mTesting.\e[0m'
 python test.py
