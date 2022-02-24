75acd0d8049658c227562d63cd043c602e730c15
Ariel Terkeltoub <ariter777@gmail.com>
2022-02-24 19:41:04 +0200

    Improved compilation script.

--- run.sh
+++ run.sh
@@ -1,7 +1,21 @@
 #!/usr/bin/env bash
 set -e
-python setup.py build_ext --inplace
+
+{ if [[ $@ > 1 && $1 == 'clean' ]]; then
+  echo $'\e[92mCleaning.\e[0m'
+  rm -vrf src/libcalculus.cpp build annotations 2>&1 | sed 's/^/    /g'
+  echo
+fi ; } &> /dev/stderr
+
+
+echo $'\e[92mBuilding.\e[0m'
+
+{ python setup.py build_ext --inplace
 mkdir -p annotations
-find . -maxdepth 1 -type f -name '*.html' | read && mv *.html annotations && echo "Annotations saved in annotations/"
+find src/ include/ -type f -name '*.html' | xargs -i -r mv {} annotations/ && echo "Annotations saved in annotations/"
 echo "Successfully compiled $(wc -l src/* include/* | tail -1 | sed -re 's/^\s*([0-9]+)\s+total\s*$/\1/g') lines."
+echo ; } 2>&1 | sed 's/^/    /g' &> /dev/stderr
+
+
+echo $'\e[92mTesting.\e[0m'
 python test.py
--- setup.py
+++ setup.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
 from setuptools import setup, Extension
 from Cython.Build import cythonize
-import shutil
+import sys
+sys.path.append("./include")
 
 setup(ext_modules=cythonize(Extension("libcalculus", ["src/libcalculus.pyx"],
                                       extra_compile_args=["-DNPY_NO_DEPRECATED_API", "-std=c++17", "-O3", "-march=native",
                                                           "-msse", "-msse2", "-mavx", "-mavx2", "-mfpmath=sse"]),
-                                      language_level=3, nthreads=4, annotate=True),
-                                      include_dirs="./include")
-
-shutil.rmtree("build")
+                                      language_level=3, nthreads=4, annotate=True))
