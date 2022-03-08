c2759123da9155de82857856e4a2fe2c3896617b
Ariel Terkeltoub <ariter777@gmail.com>
2022-03-08 14:48:21 +0200

    Added derivatives for RealFunction and Contour.

--- include/CCalculus.h
+++ include/CCalculus.h
@@ -2,8 +2,11 @@
 #include "CFunction.h"
 
 namespace libcalculus {
+    template<typename Dom, typename Ran>
+    CFunction<Dom, Ran> Derivative(CFunction<Dom, Ran> const &f, REAL const tol, REAL const radius);
+
     template<typename Dom, typename Ran, typename ContDom>
-    Ran Integrate(CFunction<Dom, Ran> const &f, CFunction<ContDom, Dom> const &contour, ContDom const start, ContDom const End, double const tol);
+    Ran Integrate(CFunction<Dom, Ran> const &f, CFunction<ContDom, Dom> const &contour, ContDom const start, ContDom const end, REAL const tol);
 
     inline size_t factorial(size_t n) noexcept {
         size_t result = 1;
--- src/CCalculus.pyx
+++ src/CCalculus.pyx
@@ -7,8 +7,9 @@ cdef extern from "CCalculus.cpp":
 
 cdef extern from "CCalculus.h" namespace "libcalculus":
   size_t factorial(const size_t n)
+  CFunction[Dom, Ran] Derivative[Dom, Ran](CFunction[Dom, Ran] f, const REAL tol, const REAL radius) except +
   Ran Integrate[Dom, Ran, ContDom](CFunction[Dom, Ran] f, CFunction[ContDom, Dom] contour,
-                                   const ContDom start, const ContDom end, const REAL tol)
+                                   const ContDom start, const ContDom end, const REAL tol) except +
 
 def integrate(f, contour, start=None, end=None, const REAL tol=1e-3):
   """"Integrate f along a contour."""
@@ -18,12 +19,26 @@ def integrate(f, contour, start=None, end=None, const REAL tol=1e-3):
   elif isinstance(f, RealFunction) and isinstance(contour, np.ndarray) and np.issubdtype(contour.dtype, np.number) and contour.shape == (2,):
       return Integrate[REAL, REAL, REAL]((<RealFunction>f).cfunction, (<RealFunction>RealFunction.Identity()).cfunction, contour[0], contour[1], tol)
 
-def derivative(ComplexFunction f, COMPLEX z0, const size_t order=1, const REAL radius=1, const REAL tol=1e-3):
-  """Calculate the derivative of f at z0, given that f does not have any singularities inside
-  a sphere of the given radius around z0."""
-  cdef Contour contour = Contour.Sphere(z0, radius)
-  cdef ComplexFunction integrand = f / ((ComplexFunction.Identity() - z0) ** (order + 1))
-  return complex(factorial(order) / (2j * M_PI)) * Integrate[COMPLEX, COMPLEX, REAL](integrand.cfunction, contour.cfunction, contour.start, contour.end, tol)
+def derivative(f, const size_t order=1, const REAL tol=1e-3, const REAL radius=1.):
+  """Returns a function object representing f's derivative."""
+  cdef ComplexFunction complex_result
+  cdef RealFunction real_result
+  cdef Contour contour_result
+  if order == 1:
+    if isinstance(f, ComplexFunction):
+      complex_result = ComplexFunction()
+      complex_result.cfunction = Derivative((<ComplexFunction>f).cfunction, tol, radius)
+      return complex_result
+    elif isinstance(f, RealFunction):
+      real_result = RealFunction()
+      real_result.cfunction = Derivative((<RealFunction>f).cfunction, tol, radius)
+      return real_result
+    elif isinstance(f, Contour):
+      contour_result = Contour(f.start, f.end)
+      contour_result.cfunction = Derivative((<Contour>f).cfunction, tol, radius)
+      return contour_result
+  else:
+    return derivative(derivative(f, order - 1, tol, radius), 1, tol, radius)
 
 def residue(ComplexFunction f, const COMPLEX z0, const REAL radius=1, const REAL tol=1e-3):
   """Calculate the residue of f around z0, given that f does not have any further singularities inside
