5d31ad1d43cfa63bad2a5ca5fe4aece81b660378
Ariel Terkeltoub <ariter777@gmail.com>
2022-03-25 11:57:18 +0300

    Updated run.sh to really fail on error.

--- run.sh
+++ run.sh
@@ -6,25 +6,23 @@ release=0
 
 function clean {
   echo $'\e[92mCleaning.\e[0m'
-  rm -vrf src/libcalculus.cpp build dist/* annotations docs/{html,doctrees} 2>&1 | sed 's/^/    /g'
+  rm -vrf src/libcalculus.cpp build dist/* annotations docs/{html,doctrees}
   echo
 }
 
 function build {
   echo $'\e[92mBuilding.\e[0m'
-  {
-    if [[ $release == 1 ]] && command -v g++-9 &> /dev/null; then
-        export CC='g++-9' CXX='g++-9' LDSHARED='g++-9 -shared'
-    fi
-    python3 setup.py build_ext --inplace
-    mkdir -p annotations
-    find src/ include/ -type f -name '*.html' | xargs -i -r mv {} annotations/ && echo "Annotations saved in annotations/"
-    echo "Successfully compiled $(wc -l src/!(libcalculus.cpp) include/* | tail -1 | sed -re 's/^\s*([0-9]+)\s+total\s*$/\1/g') lines."
-    cd docs
-    make html
-    cd - &> /dev/null
-    echo ;
-  } 2>&1 | sed 's/^/    /g' &> /dev/stderr
+  if [[ $release == 1 ]] && command -v g++-9 &> /dev/null; then
+      export CC='g++-9' CXX='g++-9' LDSHARED='g++-9 -shared'
+  fi
+  python3 setup.py build_ext --inplac
+  mkdir -p annotations
+  find src/ include/ -type f -name '*.html' | xargs -i -r mv {} annotations/ && echo "Annotations saved in annotations/"
+  echo "Successfully compiled $(wc -l src/!(libcalculus.cpp) include/* | tail -1 | sed -re 's/^\s*([0-9]+)\s+total\s*$/\1/g') lines."
+  cd docs
+  make html
+  cd - &> /dev/null
+  echo;
 }
 
 function run_tests {
